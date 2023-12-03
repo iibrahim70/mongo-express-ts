869c010cae54a03855e79ddd45ea8850ce2ed3b0
iibrahim70 <aviknuhash70@gmail.com>
2023-12-03 21:49:08 +0600

    added user interface

--- dist/app.js
+++ dist/app.js
@@ -4,12 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
 const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
 // parsers
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+// default route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 exports.default = app;
