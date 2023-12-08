f9d2059747281f99f6eba1100ce85e0087f882d9
iibrahim70 <aviknuhash70@gmail.com>
2023-12-08 12:43:51 +0600

    added all users get route

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,11 +1,17 @@
 import { IUser } from './user.interface';
 import { User } from './user.model';
 
-const createUserIntoDb = async (user: IUser) => {
+const createUserIntoDB = async (user: IUser) => {
   const result = await User.create(user);
   return result;
 };
 
+const getAllUsersFromDB = async () => {
+  const result = await User.find()
+  return result;
+}
+
 export const UserServices = {
-  createUserIntoDb,
+  createUserIntoDB,
+  getAllUsersFromDB
 };
