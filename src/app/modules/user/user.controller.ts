062e59806c5ae41630db9a3753e83949574262da
iibrahim70 <iibrahiim.dev@gmail.com>
2023-12-08 12:43:51 +0600

    added all users get route

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -4,7 +4,7 @@ import { UserServices } from './user.service';
 const createUser = async (req: Request, res: Response) => {
   try {
     const user = req.body;
-    const result = await UserServices.createUserIntoDb(user);
+    const result = await UserServices.createUserIntoDB(user);
     res.status(200).json({
       success: true,
       message: 'User created successfully!',
@@ -22,6 +22,27 @@ const createUser = async (req: Request, res: Response) => {
   }
 };
 
+const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const result = await UserServices.getAllUsersFromDB();
+    res.status(200).json({
+      success: true,
+      message: 'Users fetched successfully!',
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Internal server error!',
+      error: {
+        code: 500,
+        description: 'Internal server error!',
+      },
+    });
+  }
+};
+
 export const UserControllers = {
   createUser,
+  getAllUsers,
 };
