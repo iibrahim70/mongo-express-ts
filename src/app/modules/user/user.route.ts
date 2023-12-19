73c27d1e438446d7e0cfd265f5b14804dda7f626
iibrahim70 <aviknuhash70@gmail.com>
2023-12-19 13:14:34 +0600

    added update route

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -5,5 +5,6 @@ const router = express.Router();
 router.post('/', UserControllers.createUser);
 router.get('/', UserControllers.getAllUsers);
 router.get('/:userId', UserControllers.getUserById);
+router.put('/:userId', UserControllers.updateUserById);
 
 export const UserRouter = router;
